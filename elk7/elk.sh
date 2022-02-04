9875471f07737239736db498f5d938ca7a39ba9c
Ghassem Tofighi <ghassem@gmail.com>
2022-02-04 06:37:47 -0500

    Create elk.sh

---
+++ elk7/elk.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt install -y apt-transport-https wget unzip
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update
+sudo apt-get install elasticsearch kibana logstash
+wget -q https://raw.githubusercontent.com/tofighi/tutorials/master/elk7/configs/elasticsearch.yml
+wget -q https://raw.githubusercontent.com/tofighi/tutorials/master/elk7/configs/kibana.yml
+sudo mv elasticsearch.yml /etc/elasticsearch/
+sudo mv kibana.yml /etc/kibana/
+sudo update-rc.d elasticsearch defaults 95 10
+sudo update-rc.d kibana defaults 95 10
+sudo -i service elasticsearch start
+sudo -i service kibana start
+echo 'export PATH="$PATH":/usr/share/logstash/bin/' >> ~/.bashrc
+source ~/.bashrc 
+
+
